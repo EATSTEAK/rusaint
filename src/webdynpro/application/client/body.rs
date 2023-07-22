f54797f9b042f8d021d1a5e01a1e165fff79561f
Hyomin Koo <me@eatsteak.dev>
2023-07-22 07:44:02 +0000

    feat: add skeleton for wdbody methods

--- src/webdynpro/application/client/body.rs
+++ src/webdynpro/application/client/body.rs
@@ -1,6 +1,22 @@
 use std::ops::{Deref, DerefMut};
+use super::SapSsrClient;
 
-pub(crate) struct WDBody<'a>(tl::VDom<'a>);
+pub(crate) struct WDBodyUpdate<'a>(tl::VDom<'a>, String);
+
+impl<'a> Deref for WDBodyUpdate<'a> {
+    type Target = tl::VDom<'a>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a> DerefMut for WDBodyUpdate<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+pub(crate) struct WDBody<'a>(tl::VDom<'a>, String);
 
 impl<'a> Deref for WDBody<'a> {
     type Target = tl::VDom<'a>;
@@ -12,5 +28,20 @@ impl<'a> Deref for WDBody<'a> {
 impl<'a> DerefMut for WDBody<'a> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
+    }
+}
+
+impl<'a> WDBody<'a> {
+
+    pub fn new(body: String) -> Result<WDBody<'static>, tl::ParseError> {
+        Ok(WDBody(tl::parse(&body, tl::ParserOptions::default())?, body))
+    }
+
+    pub fn parse_sap_ssr_client(&self) -> SapSsrClient {
+        SapSsrClient{ action: todo!(), charset: todo!(), wd_secure_id: todo!(), app_name: todo!(), use_beacon: todo!() }
+    }
+
+    pub fn apply(&mut self, update: WDBodyUpdate) {
+        todo!("Implement WebDynpro dom update logic")
     }
 }
