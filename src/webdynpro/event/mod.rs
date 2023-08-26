6d22bd14cbf90d5c430cd2757aad022bdb5fc33d
Hyomin Koo <me@eatsteak.dev>
2023-08-26 06:33:12 +0000

    feat: implement `webdynpro::event::escape_str`

--- src/webdynpro/event/mod.rs
+++ src/webdynpro/event/mod.rs
@@ -9,14 +9,26 @@ pub const EVENT_DATA_COLON: &str = "~E004";
 pub const EVENT_DATA_COMMA: &str = "~E005";
 
 
-pub fn escape<'a>(text: &'a str) -> Cow<'a, str> {
+pub fn escape_str<'a>(text: &'a str) -> Cow<'a, str> {
 
     let bytes = text.as_bytes();
 
     let mut owned = None;
 
     for pos in 0..bytes.len() {
-        
+        let special = match bytes[pos] {
+            b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'-' | b'.' | b'_' => None,
+            _ => Some(bytes[pos])
+        };
+
+        if let Some(s) = special {
+            if owned.is_none() {
+                owned = Some(bytes[0..pos].to_owned());
+            }
+            owned.as_mut().unwrap().extend_from_slice(format!("~{:02x}", s).as_bytes());
+        } else if let Some(owned) = owned.as_mut() {
+            owned.push(bytes[pos]);
+        }
     }
 
     if let Some(owned) = owned {
@@ -56,3 +68,6 @@ impl<'a> WDEvent<'a> {
 
 mod ucf_parameters;
 pub(crate) mod event_queue;
+
+#[cfg(test)]
+mod test;
