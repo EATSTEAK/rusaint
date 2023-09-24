ed1ad1b5a50b07ec132dc6a0e7ef48ff05e500ac
Hyomin Koo <me@eatsteak.dev>
2023-09-24 04:45:37 +0000

    feat: create macro package

---
+++ macro/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Element, attributes(lsdata, lsevents, ignore))]
+pub fn derive_element(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    TokenStream::new()
+}
