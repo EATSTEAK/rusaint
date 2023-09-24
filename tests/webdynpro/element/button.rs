81a93b1de94062ebe85e332645c10246eb0ecb19
Hyomin Koo <me@eatsteak.dev>
2023-09-24 08:07:54 +0000

    feat: create EventTestSuite application for testing elements

--- tests/webdynpro/element/button.rs
+++ tests/webdynpro/element/button.rs
@@ -1,1 +1,26 @@
+use rusaint::{
+    element_ref,
+    webdynpro::element::{button::Button, link::Link},
+};
 
+use super::EventTestSuite;
+
+impl<'a> EventTestSuite {
+    element_ref! {
+        /* Button Test */
+        LINK_TO_BUTTON: Link<'a> = "WDR_TEST_EVENTS.ID_0001:MAIN.TOBUTTON",
+        TEST_BUTTON: Button<'a> = "WDR_TEST_EVENTS.ID_0001:BUTTON.BUTTON1",
+    }
+
+    async fn test_button() {}
+}
+
+#[tokio::test]
+async fn test_button_events() {
+    if let Some(token) = std::env::var("SSO_TOKEN") {
+        let suite = EventTestSuite::new(token).await.unwrap();
+        suite.test_button()
+    } else {
+        panic!("No SSO_TOKEN Enivrionment Variable supplied, terminate.")
+    }
+}
--- tests/webdynpro/element/mod.rs
+++ tests/webdynpro/element/mod.rs
@@ -1,6 +1,7 @@
+use anyhow::Result;
 use std::ops::{Deref, DerefMut};
 
-use rusaint::{definitions::BasicUSaintApplication, element_ref};
+use rusaint::definitions::BasicUSaintApplication;
 
 pub(crate) struct EventTestSuite(BasicUSaintApplication);
 
@@ -20,18 +21,11 @@ impl<'a> DerefMut for EventTestSuite {
 impl<'a> EventTestSuite {
     const APP_NAME: &str = "WDR_TEST_EVENTS";
 
-    element_ref! {
-        PERIOD_YEAR: ComboBox<'a> = "ZCMW_PERIOD_RE.ID_A61C4ED604A2BFC2A8F6C6038DE6AF18:VIW_MAIN.PERYR",
-        PERIOD_ID: ComboBox<'a> = "ZCMW_PERIOD_RE.ID_A61C4ED604A2BFC2A8F6C6038DE6AF18:VIW_MAIN.PERID",
-        TABLE_ROWS: ComboBox<'a> = "ZCMW2100.ID_0001:VIW_MODULES.ROWS",
-        TABSTRIP: TabStrip<'a> = "ZCMW2100.ID_0001:VIW_MAIN.MODULE_TABSTRIP",
-        BUTTON_EDU: Button<'a> = "ZCMW2100.ID_0001:VIW_MAIN.BUTTON_EDU",
-        MAIN_TABLE: SapTable<'a> = "SALV_WD_TABLE.ID_DE0D9128A4327646C94670E2A892C99C:VIEW_TABLE.SALV_WD_UIE_TABLE"
-    }
-
-    pub async fn new() -> Result<EventTestSuite> {
+    pub async fn new(token: &str) -> Result<EventTestSuite> {
         Ok(EventTestSuite(
-            BasicUSaintApplication::new(Self::APP_NAME).await?,
+            BasicUSaintApplication::with_auth(Self::APP_NAME, token).await?,
         ))
     }
 }
+
+mod button;
