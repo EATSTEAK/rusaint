0702d465631fc6920edf35d7b3e5aed83bf81229
Hyomin Koo <me@eatsteak.dev>
2023-09-29 01:28:52 +0000

    feat: get ListBox of ComboBox

--- src/webdynpro/element/combo_box.rs
+++ src/webdynpro/element/combo_box.rs
@@ -4,9 +4,9 @@ use std::{borrow::Cow, cell::OnceCell};
 use indexmap::IndexMap;
 use serde::Deserialize;
 
-use crate::webdynpro::event::Event;
+use crate::webdynpro::{event::Event, error::ElementError, application::client::body::Body};
 
-use super::{Element, ElementDef, EventParameterMap};
+use super::{Element, ElementDef, EventParameterMap, Elements};
 
 #[derive(Debug)]
 pub struct ComboBox<'a> {
@@ -128,6 +128,13 @@ impl<'a> ComboBox<'a> {
             lsdata: OnceCell::new(),
             lsevents: OnceCell::new(),
         }
+    }
+
+    pub fn item_list_box(&self, body: &'a Body) -> Result<Elements> {
+        let listbox_id = self.lsdata().and_then(|lsdata| {lsdata.item_list_box_id.as_ref()}).ok_or(ElementError::InvalidLSData)?;
+        let selector = scraper::Selector::parse(format!(r#"[id="{}"]"#, listbox_id).as_str()).or(Err(ElementError::InvalidId))?;
+        let elem = body.document().select(&selector).next().ok_or(ElementError::NoSuchElement)?;
+        Ok(Elements::dyn_elem(elem)?)
     }
 
     pub fn select(&self, key: &str, by_enter: bool) -> Result<Event> {
