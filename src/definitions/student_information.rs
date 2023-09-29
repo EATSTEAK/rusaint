a329f4ca85f4c0feea01d3dc1e07655a2bc228be
Hyomin Koo <me@eatsteak.dev>
2023-09-29 10:25:16 +0000

    feat: examine elements of StudentInformation app

--- src/definitions/student_information.rs
+++ src/definitions/student_information.rs
@@ -21,9 +21,32 @@ impl<'a> DerefMut for StudentInformation {
 impl StudentInformation {
     const APP_NAME: &str = "ZCMW1001n";
 
-    pub async fn new() -> Result<StudentInformation> {
+    pub async fn new(id: &str, token: &str) -> Result<StudentInformation> {
         Ok(StudentInformation(
-            BasicUSaintApplication::new(Self::APP_NAME).await?,
+            BasicUSaintApplication::with_auth(Self::APP_NAME, id, token).await?,
         ))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use dotenv::dotenv;
+    use crate::{webdynpro::element::Elements, definitions::student_information::StudentInformation, utils::obtain_ssu_sso_token};
+
+    #[tokio::test]
+    async fn examine_elements() {
+        dotenv().ok();
+        let id = std::env::var("SSO_ID").unwrap();
+        let password = std::env::var("SSO_PASSWORD").unwrap();
+        let token = obtain_ssu_sso_token(&id, &password).await.unwrap();
+        let mut app = StudentInformation::new(&id, &token).await.unwrap();
+        app.load_placeholder().await.unwrap();
+        let ct_selector = scraper::Selector::parse("[ct]").unwrap();
+        for elem_ref in app.body().document().select(&ct_selector) {
+            let elem = Elements::dyn_elem(elem_ref);
+            if let Ok(elem) = elem {
+                println!("{:?}", elem);
+            }
+        }
     }
 }
