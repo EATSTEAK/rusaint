94136868e1df5a9a014d33dbf12a83b8a6d3c706
Hyomin Koo <me@eatsteak.dev>
2023-10-07 11:15:28 +0000

    docs: `webdynpro` module

--- src/webdynpro/mod.rs
+++ src/webdynpro/mod.rs
@@ -1,4 +1,8 @@
+/// WebDynpro 페이지의 기본 애플리케이션 및 페이지 업데이트 기능 모듈
 pub mod application;
+/// WebDynpro 페이지를 구성하는 엘리먼트 모듈
 pub mod element;
+/// WebDynpro 클라이언트 작업 중 발생할 수 있는 오류의 모듈
 pub mod error;
+/// WebDynpro 엘리먼트가 발생시키는 이벤트 처리를 위한 모듈
 pub mod event;
