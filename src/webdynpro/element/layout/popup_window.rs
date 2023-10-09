bf0f364c184142eac10937b48155f4a9de1ceecd
Hyomin Koo <me@eatsteak.dev>
2023-10-09 03:29:43 +0000

    feat: lsdata for PopupWindow

--- src/webdynpro/element/layout/popup_window.rs
+++ src/webdynpro/element/layout/popup_window.rs
@@ -1,5 +1,6 @@
 use std::{borrow::Cow, cell::OnceCell, collections::HashMap};
 
+use crate::webdynpro::element::property::{Mode, QuickViewDesign};
 use crate::webdynpro::error::WebDynproError;
 use crate::webdynpro::event::Event;
 
@@ -9,38 +10,24 @@ use crate::webdynpro::element::{define_element_interactable, Interactable};
 define_element_interactable! {
     PopupWindow<"PW", "PopupWindow"> {},
     PopupWindowLSData {
-        value: String => "0",
-        show_help_button: bool => "1",
-        // This field originally named "type"
-        input_field_type: String => "2",
-        visibility: String => "3",
-        label_text: String => "4",
-        hide_field_help: bool => "5",
-        container_width_set: bool => "6",
-        ime_mode: String => "7",
-        auto_complete: bool => "8",
-        format_string: String => "9",
-        show_help_button_always: bool => "10",
-        date_picker_start_ref_id: String => "11",
-        access_key: String => "12",
-        display_as_text: bool => "13",
-        text_style: String => "14",
-        spinner_increment: i32 => "15",
-        spinner_bounds_check: bool => "16",
-        spinner_max: i32 => "17",
-        spinner_min: i32 => "18",
-        sap_table_field_design: String => "19",
-        validation_trigger: String => "20",
-        tab_behaviour: String => "21",
-        semantic_color: String => "22",
-        embedding_behaviour: String => "23",
-        field_help_floating: bool => "24",
-        first_day_of_week: i32 => "25",
-        custom_data: String => "26",
-        custom_style: String => "27",
-        field_help_embedding: bool => "28",
-        labelled_by: String => "29",
-        described_by: String => "30",
+        is_resizable: bool => "0",
+        has_close_button: bool => "1",
+        x: String => "2",
+        y: String => "3",
+        width: String => "4",
+        height: String => "5",
+        window_size: bool => "6",
+        default_button_id: String => "7",
+        hotkeys_id: String => "8",
+        override_minimum_size: bool => "9",
+        is_maximized: bool => "10",
+        has_help_button: bool => "11",
+        mode: Mode => "12",
+        custom_data: String => "13",
+        custom_style: String => "14",
+        no_content_scrolling: bool => "15",
+        context_menu_event: bool => "16",
+        design: QuickViewDesign => "17"
     }
 }
 
