91b06bdedcbb98ca55e6bd9183925673c6a4bf35
Hyomin Koo <me@eatsteak.dev>
2023-11-18 07:26:21 +0000

    feat: base implementation of uniffi

--- rusaint-uniffi/src/lib.rs
+++ rusaint-uniffi/src/lib.rs
@@ -1,5 +1,16 @@
 use std::sync::Arc;
 
+#[derive(thiserror::Error, Debug, uniffi::Error)]
+#[uniffi(flat_error)]
+pub enum RusaintError {
+    /// 내부 WebDynpro 엔진 오류
+    #[error("WebDynpro engine error: {0}")]
+    WebDynproError(#[from] rusaint::webdynpro::error::WebDynproError),
+    /// 숭실대학교 SSO 로그인 오류
+    #[error("Failed to login with ssu sso: {0}")]
+    SsoLoginError(#[from] rusaint::SsuSsoError),
+}
+
 #[derive(uniffi::Object)]
 pub struct USaintSession(rusaint::USaintSession);
 
@@ -9,6 +20,16 @@ impl USaintSession {
     pub fn new() -> Arc<USaintSession> {
         Arc::new(USaintSession(rusaint::USaintSession::anonymous()))
     }
+}
+
+#[uniffi::export]
+pub async fn obtain_ssu_sso_token(id: String, password: String) -> Result<String, RusaintError> {
+    Ok(rusaint::obtain_ssu_sso_token(&id, &password).await?)
+}
+
+mod application {
+    #[derive(uniffi::Object)]
+    pub struct CourseGrades(rusaint::application::course_grades::CourseGrades);
 }
 
 uniffi::setup_scaffolding!();
