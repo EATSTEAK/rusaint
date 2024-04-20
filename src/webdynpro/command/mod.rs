f0082ee425538a70f535286612e3696f542a12f7
Hyomin Koo <me@eatsteak.dev>
2024-04-20 10:32:47 +0000

    chore: import cleanup

--- src/webdynpro/command/mod.rs
+++ src/webdynpro/command/mod.rs
@@ -1,7 +1,4 @@
-use super::{
-    client::{EventProcessResult, WebDynproClient},
-    error::WebDynproError,
-};
+use super::{client::WebDynproClient, error::WebDynproError};
 
 /// WebDynpro 클라이언트를 조작하는 명령
 pub trait WebDynproCommand {
@@ -11,7 +8,6 @@ pub trait WebDynproCommand {
     /// 해당 명령을 주어진 클라이언트에 대해 실행합니다.
     async fn dispatch(&self, client: &mut WebDynproClient) -> Result<Self::Result, WebDynproError>;
 }
-
 
 /// 엘리먼트 관련 명령
 pub mod element;
