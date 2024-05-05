30dbce4a12960d23ce9b0f9039a9729586f4aa0d
Hyomin Koo <me@eatsteak.dev>
2024-05-05 09:15:37 +0000

    feat: create `SapTableHeader`

---
+++ src/webdynpro/element/complex/sap_table/header.rs
@@ -0,0 +1,128 @@
+use std::ops::Index;
+
+use scraper::ElementRef;
+
+use crate::webdynpro::{
+    client::body::Body, element::definition::ElementDefinition, error::{ElementError, WebDynproError}
+};
+
+use super::{
+    cell::{SapTableCellDefWrapper, SapTableCellWrapper},
+    property::{SapTableRowType, SapTableSelectionState},
+    SapTableDef,
+};
+
+/// [`SapTable`]의 행
+#[derive(Clone, custom_debug_derive::Debug)]
+#[allow(unused)]
+pub struct SapTableHeader<'a> {
+    table_def: SapTableDef,
+    #[debug(skip)]
+    elem_ref: ElementRef<'a>,
+    cells: Vec<SapTableCellDefWrapper>,
+    row_index: Option<u32>,
+    user_data: Option<String>,
+    drag_data: Option<String>,
+    drop_target_info: Option<String>,
+    parent_drop_target_info: Option<String>,
+    selection_state: SapTableSelectionState,
+}
+
+impl<'a> SapTableHeader<'a> {
+    pub(super) fn new(
+        table_def: SapTableDef,
+        header_ref: ElementRef<'a>,
+    ) -> Result<SapTableHeader<'a>, ElementError> {
+        let row = header_ref.value();
+        let subct_selector = scraper::Selector::parse("[subct]").unwrap();
+        let subcts = header_ref.select(&subct_selector);
+        let cells = subcts
+            .filter_map(|subct_ref| {
+                SapTableCellDefWrapper::dyn_cell_def(table_def.clone(), subct_ref)
+            })
+            .collect::<Vec<SapTableCellDefWrapper>>();
+        let row_type = row
+        .attr("rt")
+        .and_then(|s| Some(s.into()))
+        .unwrap_or(SapTableRowType::default());
+        if !matches!(row_type, SapTableRowType::Header) {
+            return Err(ElementError::InvalidContent { element: table_def.id().to_string(), content: "Header of table is invalid".to_string() })
+        }
+        Ok(SapTableHeader {
+            table_def,
+            elem_ref: header_ref,
+            cells,
+            row_index: row.attr("rr").and_then(|s| s.parse::<u32>().ok()),
+            user_data: row.attr("uDat").and_then(|s| Some(s.to_owned())),
+            drag_data: row.attr("ddData").and_then(|s| Some(s.to_owned())),
+            drop_target_info: row.attr("ddDti").and_then(|s| Some(s.to_owned())),
+            parent_drop_target_info: row.attr("ddPDti").and_then(|s| Some(s.to_owned())),
+            selection_state: row
+                .attr("sst")
+                .and_then(|s| Some(s.into()))
+                .unwrap_or(SapTableSelectionState::default()),
+        })
+    }
+
+    /// 행 내부 셀의 개수를 반환합니다.
+    pub fn len(&self) -> usize {
+        self.cells.len()
+    }
+
+    /// 행 내부 셀 정의의 [`Iterator`]를 반환합니다.
+    pub fn iter(&self) -> impl Iterator<Item = &SapTableCellDefWrapper> + ExactSizeIterator {
+        self.cells.iter()
+    }
+
+    /// 행 내부 셀 엘리먼트의 [`Iterator`]를 반환합니다.
+    pub fn iter_value(
+        &'a self,
+        body: &'a Body,
+    ) -> impl Iterator<Item = Result<SapTableCellWrapper<'a>, WebDynproError>> + ExactSizeIterator
+    {
+        self.cells.iter().map(|def| def.clone().from_body(body))
+    }
+
+    /// 원본 [`SapTable`]의 [`ElementDef`]를 반환합니다.
+    pub fn table_def(&self) -> SapTableDef {
+        self.table_def.clone()
+    }
+
+    /// 인덱스를 반환합니다.
+    pub fn row_index(&self) -> Option<u32> {
+        self.row_index
+    }
+
+    /// 유저 데이터를 반환합니다.
+    pub fn user_data(&self) -> Option<&str> {
+        self.user_data.as_ref().map(|x| x.as_str())
+    }
+
+    /// 드레그 데이터를 반환합니다.
+    pub fn drag_data(&self) -> Option<&str> {
+        self.drag_data.as_ref().map(|x| x.as_str())
+    }
+
+    /// 드롭 타겟 정보를 반환합니다.
+    pub fn drop_target_info(&self) -> Option<&str> {
+        self.drop_target_info.as_ref().map(|x| x.as_str())
+    }
+
+    /// 부모의 드롭 타겟 정보를 반환합니다.
+    pub fn parent_drop_target_info(&self) -> Option<&str> {
+        self.parent_drop_target_info.as_ref().map(|x| x.as_str())
+    }
+
+    /// 선택 상태를 반환합니다.
+    pub fn selection_state(&self) -> SapTableSelectionState {
+        self.selection_state
+    }
+}
+
+impl<'a> Index<usize> for SapTableHeader<'a> {
+    type Output = SapTableCellDefWrapper;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.cells[index]
+    }
+}
