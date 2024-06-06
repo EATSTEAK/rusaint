6148b946b90667315f89d7d694a8595621711360
Hyomin Koo <me@eatsteak.dev>
2024-06-06 05:19:07 +0000

    feat: implement `SapTableVerticalScrollCommand`

--- src/webdynpro/element/complex/sap_table/mod.rs
+++ src/webdynpro/element/complex/sap_table/mod.rs
@@ -121,6 +121,36 @@ impl<'a> SapTable<'a> {
         ]);
         self.fire_event("CellSelect".to_string(), parameters)
     }
+
+    /// 테이블을 상하로 스크롤하는 이벤트를 반환합니다.
+    pub fn vertical_scroll(
+        &self,
+        first_visible_item_index: u32,
+        cell_id: &str,
+        access_type: &str,
+        selection_follow_focus: bool,
+        shift: bool,
+        ctrl: bool,
+        alt: bool,
+    ) -> Result<Event, WebDynproError> {
+        let parameters: HashMap<String, String> = HashMap::from([
+            ("Id".to_string(), self.id.clone().to_string()),
+            (
+                "FirstVisibleItemIndex".to_string(),
+                first_visible_item_index.to_string(),
+            ),
+            ("CellId".to_string(), cell_id.to_owned()),
+            ("AccessType".to_string(), access_type.to_string()),
+            (
+                "SelectionFollowFocus".to_string(),
+                selection_follow_focus.to_string(),
+            ),
+            ("Shift".to_string(), shift.to_string()),
+            ("Ctrl".to_string(), ctrl.to_string()),
+            ("Alt".to_string(), alt.to_string()),
+        ]);
+        self.fire_event("VerticalScroll".to_string(), parameters)
+    }
 }
 
 mod body;
