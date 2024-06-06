ab6b88c3171783fb275a1beea860603483fae2d1
Hyomin Koo <me@eatsteak.dev>
2024-06-06 05:38:21 +0000

    test: find noting test in `CourseSchedule`

--- tests/application/course_schedule.rs
+++ tests/application/course_schedule.rs
@@ -4,6 +4,7 @@ use rusaint::{
         USaintClientBuilder,
     },
     model::SemesterType,
+    ApplicationError, RusaintError,
 };
 use serial_test::serial;
 
@@ -27,4 +28,27 @@ async fn find_major() {
         println!("{:?}", lecture);
     }
     assert!(true);
+}
+
+#[tokio::test]
+#[serial]
+async fn find_nothing() {
+    let session = get_session().await.unwrap();
+    let mut app = USaintClientBuilder::new()
+        .session(session)
+        .build_into::<CourseSchedule>()
+        .await
+        .unwrap();
+    let category = LectureCategory::find_by_lecture("내가A+받는강의");
+    let Some(err) = app
+        .find_lectures(2023, SemesterType::Two, category)
+        .await
+        .err()
+    else {
+        panic!("this lecture query should return error");
+    };
+    assert!(matches!(
+        err,
+        RusaintError::ApplicationError(ApplicationError::NoLectureResult)
+    ));
 }
