1dd19a9beb9cb08daeedfd1ae5dac440e1117cf4
Hyomin Koo <me@eatsteak.dev>
2024-06-06 02:38:27 +0000

    test: implements no information error tests

--- tests/application/chapel.rs
+++ tests/application/chapel.rs
@@ -1,4 +1,8 @@
-use rusaint::{application::{chapel::Chapel, USaintClientBuilder}, model::SemesterType};
+use rusaint::{
+    application::{chapel::Chapel, USaintClientBuilder},
+    model::SemesterType,
+    ApplicationError, RusaintError,
+};
 use serial_test::serial;
 
 use crate::get_session;
@@ -13,5 +17,23 @@ async fn chapel() {
         .await
         .unwrap();
     let info = app.information(2022, SemesterType::Two).await.unwrap();
+    println!("{:?}", info);
+}
+
+#[tokio::test]
+#[serial]
+async fn no_chapel() {
+    let session = get_session().await.unwrap();
+    let mut app = USaintClientBuilder::new()
+        .session(session)
+        .build_into::<Chapel>()
+        .await
+        .unwrap();
+    let info = app.information(2024, SemesterType::Two).await.unwrap_err();
+    assert!(matches!(
+        info,
+        RusaintError::ApplicationError(ApplicationError::NoChapelInformation)
+    ));
+    println!("{:?}", info);
     println!("{:?}", info);
 }
--- tests/application/personal_course_schedule.rs
+++ tests/application/personal_course_schedule.rs
@@ -1,6 +1,7 @@
 use rusaint::{
     application::{personal_course_schedule::PersonalCourseSchedule, USaintClientBuilder},
     model::SemesterType,
+    ApplicationError, RusaintError,
 };
 use serial_test::serial;
 
@@ -16,5 +17,22 @@ async fn schedule() {
         .await
         .unwrap();
     let info = app.schedule(2022, SemesterType::Two).await.unwrap();
+    println!("{:?}", info);
+}
+
+#[tokio::test]
+#[serial]
+async fn no_schedule() {
+    let session = get_session().await.unwrap();
+    let mut app = USaintClientBuilder::new()
+        .session(session)
+        .build_into::<PersonalCourseSchedule>()
+        .await
+        .unwrap();
+    let info = app.schedule(2024, SemesterType::Two).await.unwrap_err();
+    assert!(matches!(
+        info,
+        RusaintError::ApplicationError(ApplicationError::NoScheduleInformation)
+    ));
     println!("{:?}", info);
 }
