4077cfda36e5345c1071ad9df4c62831a0f77fbc
Hyomin Koo <me@eatsteak.dev>
2024-08-30 07:23:07 +0900

    refactor: remove `WebDynproReadCommand` and make commands do reading task only

--- rusaint/src/webdynpro/command/mod.rs
+++ rusaint/src/webdynpro/command/mod.rs
@@ -1,7 +1,4 @@
-use super::{
-    client::WebDynproClient,
-    error::WebDynproError,
-};
+use super::error::WebDynproError;
 use crate::webdynpro::element::parser::ElementParser;
 
 /// WebDynpro 클라이언트를 조작하는 명령
@@ -10,14 +7,7 @@ pub trait WebDynproCommand {
     type Result;
 
     /// 해당 명령을 주어진 클라이언트에 대해 실행합니다.
-    #[allow(async_fn_in_trait)]
-    async fn dispatch(&self, client: &mut WebDynproClient) -> Result<Self::Result, WebDynproError>;
-}
-
-/// WebDynpro 클라이언트 내부 페이지에서 데이터를 가져오는 명령
-pub trait WebDynproReadCommand: WebDynproCommand {
-    /// 해당 명령을 주어진 클라이언트에 대해 실행합니다.
-    fn read(&self, parser: &ElementParser) -> Result<Self::Result, WebDynproError>;
+    fn dispatch(&self, parser: &ElementParser) -> Result<Self::Result, WebDynproError>;
 }
 
 /// 엘리먼트 관련 명령
