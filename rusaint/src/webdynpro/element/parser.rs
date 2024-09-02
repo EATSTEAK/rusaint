9e610a7ee42dae02ceb9f0dd9c59475dc8f5626f
Hyomin Koo <me@eatsteak.dev>
2024-09-02 18:01:52 +0900

    feat: create `ElementParser::subelement_from_ref`

--- rusaint/src/webdynpro/element/parser.rs
+++ rusaint/src/webdynpro/element/parser.rs
@@ -1,3 +1,4 @@
+use crate::webdynpro::element::sub::definition::SubElementDefinition;
 use crate::webdynpro::element::{definition::ElementDefinition, Element};
 use crate::webdynpro::error::{ElementError, WebDynproError};
 
@@ -8,6 +9,19 @@ impl ElementParser {
         &self,
         definition: &T,
     ) -> Result<T::Element, WebDynproError> {
+        let selector = definition.selector()?;
+        let element_ref = self
+            .0
+            .select(&selector)
+            .next()
+            .ok_or(ElementError::InvalidId(definition.id().to_string()))?;
+        T::Element::from_ref(&definition, element_ref)
+    }
+
+    pub fn subelement_from_def<'body, T: SubElementDefinition<'body>>(
+        &self,
+        definition: &T,
+    ) -> Result<T::SubElement, WebDynproError> {
         let selector = definition.selector()?;
         let element_ref = self
             .0
