cff8fcd23acfdc0bfbefe42b54e64e10250b2849
Hyomin Koo <me@eatsteak.dev>
2024-09-06 18:29:26 +0900

    docs: for `WebDynproCommandExecutor`

--- rusaint/src/webdynpro/command/mod.rs
+++ rusaint/src/webdynpro/command/mod.rs
@@ -10,7 +10,9 @@ pub trait WebDynproCommand {
     fn dispatch(&self, parser: &ElementParser) -> Result<Self::Result, WebDynproError>;
 }
 
+/// 명령을 실행할 수 있는 주체에 명령 실행을 할 수 있는 메소드를 추가
 pub trait WebDynproCommandExecutor {
+    /// WebDynpro 엘리먼트를 읽을 수 있는 파서에 읽기 명령을 전송합니다.
     fn read<T: WebDynproCommand>(&self, command: T) -> Result<T::Result, WebDynproError>;
 }
 
