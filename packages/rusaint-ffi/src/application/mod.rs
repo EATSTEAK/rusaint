78332352da6b02941bb30fbeed9e76ab08e53354
EATSTEAK <me@eatsteak.dev>
2024-10-20 09:14:27 +0900

    feat: ffi implementation of `PersonalCourseScheduleApplication`

--- packages/rusaint-ffi/src/application/mod.rs
+++ packages/rusaint-ffi/src/application/mod.rs
@@ -3,3 +3,4 @@ pub mod course_grades;
 pub mod course_schedule;
 mod graduation_requirments;
 mod lecture_assessment;
+mod personal_course_schedule;
---
+++ packages/rusaint-ffi/src/application/personal_course_schedule.rs
@@ -0,0 +1,49 @@
+use std::sync::Arc;
+
+use rusaint::{
+    model::SemesterType,
+};
+use tokio::sync::RwLock;
+use rusaint::application::personal_course_schedule::model::PersonalCourseSchedule;
+use crate::{error::RusaintError, session::USaintSession};
+
+#[derive(uniffi::Object)]
+pub struct PersonalCourseScheduleApplication(
+    RwLock<rusaint::application::personal_course_schedule::PersonalCourseScheduleApplication>,
+);
+
+#[uniffi::export(async_runtime = "tokio")]
+impl PersonalCourseScheduleApplication {
+
+    /// 해당 학기의 시간표 정보를 가져옵니다.
+    pub async fn schedule(
+        &self,
+        year: u32,
+        semester: SemesterType,
+    ) -> Result<PersonalCourseSchedule, RusaintError> {
+        Ok(self.0.write().await.schedule(year, semester).await?)
+    }
+}
+
+#[derive(uniffi::Object)]
+pub struct PersonalCourseScheduleApplicationBuilder {}
+
+#[uniffi::export(async_runtime = "tokio")]
+impl PersonalCourseScheduleApplicationBuilder {
+    #[uniffi::constructor]
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub async fn build(
+        &self,
+        session: Arc<USaintSession>,
+    ) -> Result<PersonalCourseScheduleApplication, RusaintError> {
+        let mut original_builder = rusaint::application::USaintClientBuilder::new();
+        original_builder = original_builder.session(session.original());
+        let original_app = original_builder
+            .build_into::<rusaint::application::personal_course_schedule::PersonalCourseScheduleApplication>()
+            .await?;
+        Ok(PersonalCourseScheduleApplication(RwLock::new(original_app)))
+    }
+}
