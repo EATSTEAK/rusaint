ce7e08868fb3900482ecdc01b53a9147865cc60a
EATSTEAK <me@eatsteak.dev>
2024-10-20 09:37:13 +0900

    fix: typo in `graduation_requirements` module

--- packages/rusaint-ffi/src/application/mod.rs
+++ packages/rusaint-ffi/src/application/mod.rs
@@ -1,7 +1,7 @@
 pub mod chapel;
 pub mod course_grades;
 pub mod course_schedule;
-mod graduation_requirments;
+mod graduation_requirements;
 mod lecture_assessment;
 mod personal_course_schedule;
 mod student_information;
