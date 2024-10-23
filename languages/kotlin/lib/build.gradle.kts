8aef3e1388c664009f85ee84b4b931aff492bb03
EATSTEAK <me@eatsteak.dev>
2024-10-23 23:27:07 +0900

    feat: automatic build for android with `rust-android-gradle`

--- languages/kotlin/lib/build.gradle.kts
+++ languages/kotlin/lib/build.gradle.kts
@@ -1,10 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    alias(libs.plugins.rust.android)
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
     namespace = "dev.eatsteak.rusaint"
+    ndkVersion = "27.2.12479018"
     compileSdk = 34
 
     defaultConfig {
@@ -32,11 +36,35 @@ android {
     }
 }
 
-dependencies {
-    implementation("net.java.dev.jna:jna:5.14.0@aar")
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+cargo {
+    module = "../../.."
+    libname = "rusaint-ffi"
+    targets = listOf("arm", "x86", "arm64", "x86_64")
+    profile = "release"
+    targetIncludes = arrayOf("librusaint_ffi.so")
+}
 
+tasks.withType<KotlinCompile> {
+    dependsOn("generateBindings")
+}
+
+task<Exec>("generateBindings") {
+    dependsOn("cargoBuild")
+    commandLine("cargo", "run", "-p", "uniffi-bindgen", "generate",
+        "./build/rustJniLibs/android/arm64-v8a/librusaint_ffi.so",
+        "--library",
+        "--language",
+        "kotlin",
+        "--no-format",
+        "--out-dir",
+        "src/main/kotlin")
+}
+
+dependencies {
+    //noinspection UseTomlInstead
+    // See: https://github.com/gradle/gradle/issues/21267
+    implementation("net.java.dev.jna:jna:5.14.0@aar")
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
