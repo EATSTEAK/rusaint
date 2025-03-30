6e1a656bc9f4a3a9f79bbe9c29e1b7539692cdd2
EATSTEAK <me@eatsteak.dev>
2025-03-30 14:46:21 +0900

    chore: ignore rn ios generated files

--- languages/react-native/ios/ReactNative.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-#import "rusaint-react-native.h"
-#endif
-
-#import "generated/RNReactNativeSpec/RNReactNativeSpec.h"
-
-@interface ReactNative : NSObject <NativeReactNativeSpec>
-
-@end
