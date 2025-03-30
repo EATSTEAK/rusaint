f202c88b5a7fb5c2415953e57632c1b801e23f24
EATSTEAK <me@eatsteak.dev>
2025-03-30 15:09:09 +0900

    feat: app with basic example

--- languages/react-native/example/src/App.tsx
+++ languages/react-native/example/src/App.tsx
@@ -1,12 +1,26 @@
+import {
+  CourseScheduleApplicationBuilder,
+  LectureCategoryBuilder,
+  SemesterType,
+  USaintSessionBuilder,
+} from '@rusaint/react-native';
 import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from '@rusaint/react-native';
+import { use } from 'react';
 
-const result = multiply(3, 7);
+const session = new USaintSessionBuilder().anonymous();
+
+const client = await new CourseScheduleApplicationBuilder().build(session);
 
 export default function App() {
+  const category = new LectureCategoryBuilder().major(
+    'IT대학',
+    '글로벌미디어학부',
+    undefined
+  );
+  let result = use(client.findLectures(2025, SemesterType.One, category));
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result: {JSON.stringify(result[0])}</Text>
     </View>
   );
 }
