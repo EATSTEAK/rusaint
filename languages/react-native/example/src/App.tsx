d9ef2da079b5ba72bd1b368afe709cae8e640b1a
Hyomin Koo <me@eatsteak.dev>
2025-05-27 05:00:20 +0900

    fix: working app example

--- languages/react-native/example/src/App.tsx
+++ languages/react-native/example/src/App.tsx
@@ -1,23 +1,42 @@
 import {
   CourseScheduleApplicationBuilder,
+  type CourseScheduleApplicationInterface,
+  Lecture,
   LectureCategoryBuilder,
   SemesterType,
   USaintSessionBuilder,
 } from '@rusaint/react-native';
 import { Text, View, StyleSheet } from 'react-native';
-import { use } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const session = new USaintSessionBuilder().anonymous();
 
-const client = await new CourseScheduleApplicationBuilder().build(session);
-
 export default function App() {
-  const category = new LectureCategoryBuilder().major(
-    'IT대학',
-    '글로벌미디어학부',
-    undefined
-  );
-  let result = use(client.findLectures(2025, SemesterType.One, category));
+  console.log('App started');
+  const clientRef = useRef<CourseScheduleApplicationInterface | null>(null);
+  const [result, setResult] = useState<Lecture[]>([]);
+  useEffect(() => {
+    (async () => {
+      console.log('Initializing client');
+      const client = await new CourseScheduleApplicationBuilder().build(
+        session
+      );
+      clientRef.current = client;
+      console.log('Client initialized');
+      const category = new LectureCategoryBuilder().major(
+        'IT대학',
+        '글로벌미디어학부',
+        undefined
+      );
+      let result = await clientRef.current?.findLectures(
+        2025,
+        SemesterType.One,
+        category
+      );
+      setResult(result || []);
+      console.log('Lectures fetched:', result);
+    })();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Result: {JSON.stringify(result[0])}</Text>
