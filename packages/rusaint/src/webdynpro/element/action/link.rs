7c185b1373aa966b9592139114daa6fa14aa3f23
EATSTEAK <me@eatsteak.dev>
2025-07-20 16:50:10 +0900

    feat(webdynpro): `Link::text` and display trait implementation

--- packages/rusaint/src/webdynpro/element/action/link.rs
+++ packages/rusaint/src/webdynpro/element/action/link.rs
@@ -1,8 +1,10 @@
 use std::{borrow::Cow, cell::OnceCell, collections::HashMap};
 
+use scraper::Node;
+
 use crate::webdynpro::{
     element::{
-        Interactable,
+        Element as _, Interactable,
         macros::define_element_interactable,
         property::{HotkeyValue, Visibility},
     },
@@ -12,7 +14,9 @@ use crate::webdynpro::{
 
 define_element_interactable! {
     #[doc = "액션을 수행하거나 링크로 이동하는 하이퍼링크"]
-    Link<"LN", "Link"> {},
+    Link<"LN", "Link"> {
+        text: OnceCell<String>,
+    },
     #[doc = "[`Link`]의 정의"]
     LinkDef,
     #[doc ="[`Link`] 내부 데이터"]
@@ -41,7 +45,28 @@ impl<'a> Link<'a> {
             element_ref,
             lsdata: OnceCell::new(),
             lsevents: OnceCell::new(),
+            text: OnceCell::new(),
         }
+    }
+
+    /// 내부 텍스트를 반환합니다.
+    pub fn text(&self) -> &str {
+        self.text.get_or_init(|| {
+            self.element_ref()
+                .children()
+                .filter_map(|node| match node.value() {
+                    Node::Text(text) => Some(text.to_string()),
+                    Node::Element(elem) => {
+                        if elem.name() == "br" {
+                            Some("\n".to_string())
+                        } else {
+                            None
+                        }
+                    }
+                    _ => None,
+                })
+                .collect::<String>()
+        })
     }
 
     /// 링크 활성화 이벤트를 반환합니다. `ctrl` 이나 `shift` 가 참일 경우 각 버튼을 누른 채로 클릭한 것으로 간주합니다.
@@ -58,5 +83,11 @@ impl<'a> Link<'a> {
         let mut parameters: HashMap<String, String> = HashMap::new();
         parameters.insert("Id".to_string(), self.id.clone().to_string());
         self.fire_event("DoubleClick".to_string(), parameters)
+    }
+}
+
+impl std::fmt::Display for Link<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.text())
     }
 }
