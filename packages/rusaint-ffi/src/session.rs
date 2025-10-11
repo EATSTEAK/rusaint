f1c38017a2d301b722d89f961a31fb5ddd9744f6
Koo Hyomin <me@eatsteak.dev>
2025-10-12 03:44:49 +0900

    feat(rusaint-ffi): `USaintSessionBuilder::from_json`, `USaintSession::to_json`

--- packages/rusaint-ffi/src/session.rs
+++ packages/rusaint-ffi/src/session.rs
@@ -19,6 +19,20 @@ impl USaintSession {
     }
 }
 
+#[uniffi::export]
+impl USaintSession {
+    /// 세션을 json 형식으로 변환합니다.
+    pub fn to_json(&self) -> Result<String, RusaintError> {
+        let mut buffer = Vec::new();
+        {
+            let mut writer = std::io::BufWriter::new(&mut buffer);
+            self.0.save_to_json(&mut writer)?;
+        }
+
+        Ok(String::from_utf8(buffer).unwrap())
+    }
+}
+
 /// [`USaintSession`]을 생성하기 위한 빌더
 #[derive(Debug, uniffi::Object)]
 pub struct USaintSessionBuilder();
@@ -67,10 +81,31 @@ impl USaintSessionBuilder {
         let original = rusaint::USaintSession::with_token(id, token).await?;
         Ok(USaintSession(Arc::new(original)))
     }
+
+    /// json 형식으로 저장된 세션을 읽어 세션을 생성합니다.
+    pub fn from_json(&self, json: &str) -> Result<USaintSession, RusaintError> {
+        let reader = std::io::BufReader::new(json.as_bytes());
+        let session = rusaint::USaintSession::from_json(reader)?;
+        Ok(USaintSession(Arc::new(session)))
+    }
 }
 
 impl Default for USaintSessionBuilder {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_session_with_json() {
+        let session = USaintSessionBuilder::new().anonymous();
+        let json = session.to_json().unwrap();
+        let session2 = USaintSessionBuilder::new().from_json(&json).unwrap();
+        let json2 = session2.to_json().unwrap();
+        assert_eq!(json, json2);
     }
 }
